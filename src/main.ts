bc1cb3e03a314cb888f959b780a9d7010a3225a2
Bartosz <bartosz.burdalski@gmail.com>
2024-09-12 22:02:25 +0200

    Delete spec files

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
