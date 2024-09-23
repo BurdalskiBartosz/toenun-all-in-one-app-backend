b8fa288b991c7351c78650595e06b167d5155180
Bartosz <bartosz.burdalski@gmail.com>
2024-09-23 02:23:53 +0200

    Apply middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
+import { AuthMiddleware } from './middleware/Auth';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/AuthGuard';
 
 @Module({
   imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('auth');
+  }
+}
